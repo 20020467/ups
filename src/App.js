362946d5c442140c3a6896f88a3aed0667c0a2d4
TuyenNguyen127 <probrocro@gmail.com>
2023-05-22 16:30:47 +0700

    Fix all bugs

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/productDetail" element={<ProductDetail/>}/>
       </Routes>
     </Router>
   );
