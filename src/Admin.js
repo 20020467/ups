5e91e30be091ea986a0a6f999cfbd3e86c465338
TuyenNguyen127 <probrocro@gmail.com>
2023-05-22 15:49:43 +0700

    add admin page

---
+++ src/Admin.js
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Firm from "./pages/admin/firm";
+import Categories from "./pages/admin/categories";
+import Product from "./pages/admin/products";
+import NewProduct from "./pages/admin/product/new";
+import Home from "./pages/admin/home";
+
+function Admin() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/admin" element={<Home/>}/>
+        <Route path="/admin/firm" element={<Firm/>}/>
+        <Route path="/admin/category" element={<Categories/>}/>
+        <Route path="/admin/products" element={<Product/>}/>
+        <Route path="/admin/products/new" element={<NewProduct/>}/>
+      </Routes>
+    </Router>
+  );
+}
+
+export default Admin;
